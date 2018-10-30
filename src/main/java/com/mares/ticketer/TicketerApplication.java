dd925fff49dcc671031a83336e12ddd6dfcd9b8f
Jan Mares <maresjan694@gmail.com>
2018-10-30 21:44:09 +0100

    init commit

---
+++ src/main/java/com/mares/ticketer/TicketerApplication.java
@@ -0,0 +1,12 @@
+package com.mares.ticketer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mares/ticketer/com/mares/ticketer/controller/TicketController.java
@@ -0,0 +1,10 @@
+package com.mares.ticketer.com.mares.ticketer.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author <a href="mailto:maresjan694@gmail.com">Jan Mares</a>, 30.10.2018
+ */
+@Controller
+public class TicketController {
+}
